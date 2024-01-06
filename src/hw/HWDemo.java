975d93d1767bda37c5d704b303fdcfabad3ba153
ivans <ivans@LAPTOP-6O43JKHC>
2024-01-06 05:17:33 +0200

    main()

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -4,4 +4,11 @@ public class HWDemo {
 
 	//Instruction: Each team member pushes at least once by adding 1 method
 	//Practice Cloning, Pulling, Pushing, Stashing
+	
+	public static void main(String[] args) { // Ivan created main()
+		
+		
+		
+	}
+	
 }

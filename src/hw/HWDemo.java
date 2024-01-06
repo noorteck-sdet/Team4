e641bbdf5e2cee0bb94ef3ed2070f5b2eebaf3e1
aaldu <aaldu@DESKTOP-26OB3J1.myfiosgateway.com>
2024-01-05 22:17:26 -0500

    main method

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -4,4 +4,10 @@ public class HWDemo {
 
 	//Instruction: Each team member pushes at least once by adding 1 method
 	//Practice Cloning, Pulling, Pushing, Stashing
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Hello from Yasmine to Team 4...");
+	}
+	
 }

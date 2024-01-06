24975c3038a2752593c77315acd6eb6147daacdd
ivans <ivans@LAPTOP-6O43JKHC>
2024-01-06 05:28:27 +0200

    Merge branch 'master' of https://github.com/noorteck-sdet/Team4.git

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -9,16 +9,11 @@ public class HWDemo {
 	//Instruction: Each team member pushes at least once by adding 1 method
 	//Practice Cloning, Pulling, Pushing, Stashing
 	
-<<<<<<< HEAD
-	public static void main(String[] args) { // Ivan created main()
-		
-		
-		
-=======
+
 	
 	public static void main(String[] args) {
 		System.out.println("Hello from Yasmine to Team 4...");
->>>>>>> branch 'master' of https://github.com/noorteck-sdet/Team4.git
+
 	}
 	
 	

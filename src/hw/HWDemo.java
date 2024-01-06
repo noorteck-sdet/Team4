a22f10da02e179c2a67ff7f197b0e96ab73e7708
Ezra ITC <Ezra ITC@Ezra.lan>
2024-01-05 22:20:34 -0500

    sneaking for homework as a starter

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -4,4 +4,8 @@ public class HWDemo {
 
 	//Instruction: Each team member pushes at least once by adding 1 method
 	//Practice Cloning, Pulling, Pushing, Stashing
+public static void main(String[] args) {
+	System.out.println(" I like the group discusion");
+	// let make fun of it
+}
 }

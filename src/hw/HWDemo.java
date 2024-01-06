9972286cb960610e861c045cafc0fe4d7b6bceae
MonicaMelo47 <alexamebu14@gmail.com>
2024-01-06 16:16:18 -0700

    Adding code MM

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -90,6 +90,24 @@ public class HWDemo {
 		 }
 		  System.out.println("Yasmine @ 3:00PM");
 		  return list;
-		
 	}
+		  // Adding code from codingBat
+
+/**Given an array of ints, return the number of 9's in the array.
+
+
+arrayCount9([1, 2, 9]) → 1
+arrayCount9([1, 9, 9]) → 2
+arrayCount9([1, 9, 9, 3, 9]) → 3
+*/
+	public int arrayCount9(int[] nums) {
+		  int count = 0;
+		  for (int i=0; i<nums.length; i++) {
+		    if (nums[i] == 9) {
+		      count++;
+		    }
+		  }
+		  return count;
+		}
+	
 }

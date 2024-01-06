3312b73d1060cf107affed9db49c27e35b602300
SINAN <SINAN@HP-ProBook-445-G7>
2024-01-05 19:17:44 -0800

    Initial Commit

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -1,6 +1,10 @@
 package hw;
 
 public class HWDemo {
+	
+	public static void main(String[] args) {
+		System.out.println("This is Sinan");
+	}
 
 	//Instruction: Each team member pushes at least once by adding 1 method
 	//Practice Cloning, Pulling, Pushing, Stashing
